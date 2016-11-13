b3005918ed36e70b4959710e448399451ad5e84d
Daniel Caaz <daniel@caaz.me>
2016-11-13 15:23:51 -0800

    remove refresh

--- src/browser.js
+++ src/browser.js
@@ -27,13 +27,6 @@ function open () {
       label: 'View',
       submenu: [
         {
-          label: 'Reload',
-          accelerator: 'CmdOrCtrl+R',
-          click (item, focusedWindow) {
-            if (focusedWindow) focusedWindow.reload()
-          }
-        },
-        {
           label: 'Toggle Developer Tools',
           accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
           click (item, focusedWindow) {
