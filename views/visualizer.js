066ed89badb8faba531e004b0831f190e94039ea
Daniel Caaz <daniel@caaz.me>
2017-01-02 15:20:34 -0800

    Add visualizer script, stop hiding the player window.

---
+++ views/visualizer.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // this is our audio tag, which plays all the music. You'll probably need it.
+  const audio = document.getElementsByTagName('AUDIO')[0]
+  // Do some visualizer shit here, I guess.
+})
