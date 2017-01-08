2223896cc05bf21133900405bf05f4e96074de95
Michael Bartlett <bartlettmic@gmail.com>
2017-01-08 06:45:35 -0700

    Spectrum init added

--- src/view/visualizer/spectrum.js
+++ src/view/visualizer/spectrum.js
@@ -1,14 +1,18 @@
+let bufferLength
+let data
 module.exports = {
-  draw(timestamp, {analyser, canvas, ctx}) {
+  init({analyser}) {
     analyser.fftSize = 256
-    const bufferLength = analyser.frequencyBinCount
-    const dataArray = new Uint8Array(bufferLength)
-    analyser.getByteFrequencyData(dataArray)
+    bufferLength = analyser.frequencyBinCount
+    data = new Uint8Array(bufferLength)
+  },
+  draw(timestamp, {analyser, canvas, ctx}) {
+    analyser.getByteFrequencyData(data)
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     const width = parseInt(bufferLength * 5 / 6, 10)
     const barWidth = ((canvas.width - width) / width)
     for(let i = 0; i < width; i++) {
-      const barHeight = canvas.height * dataArray[i] / 256
+      const barHeight = canvas.height * data[i] / 256
       let dicks = (i / bufferLength) * 360
       dicks = (dicks + (timestamp / 20)) % 360
       ctx.fillStyle = 'hsl(' + dicks + ', 100%,50%)'
