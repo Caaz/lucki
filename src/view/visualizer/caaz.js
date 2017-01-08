f000007303f200b83821624abef3c2bf21a533af
Daniel Caaz <daniel@caaz.me>
2017-01-08 05:08:22 -0800

    Adele approved visualizer. Visualizers can define an init block now.

--- src/view/visualizer/caaz.js
+++ src/view/visualizer/caaz.js
@@ -1,29 +1,34 @@
+let bufferLength
+let data
+function spectrum(canvas, ctx, width, offset) {
+  ctx.beginPath()
+  ctx.moveTo(0, canvas.height + offset)
+  for(let i = 0; i < bufferLength; i++) {
+    const height = canvas.height * data[i] / 256
+    ctx.lineTo((width * i), canvas.height - 1 - height + offset)
+  }
+  ctx.lineTo(canvas.width, canvas.height + offset)
+  ctx.fill()
+}
 module.exports = {
-  draw(timestamp, {analyser, canvas, ctx}) {
+  init({analyser}) {
     analyser.fftSize = 256
-    const bufferLength = analyser.frequencyBinCount
-    const dataArray = new Uint8Array(bufferLength)
-    analyser.getByteFrequencyData(dataArray)
-
-    // const frame = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    bufferLength = analyser.frequencyBinCount
+    data = new Uint8Array(bufferLength)
+  },
+  draw(timestamp, {analyser, canvas, ctx}) {
+    analyser.getByteFrequencyData(data)
+    // draw previous frame
     ctx.drawImage(canvas, 0, -1, canvas.width, canvas.height)
-    // ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // darken it
     ctx.fillStyle = 'rgba(0,0,0,0.05)'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     const width = parseInt(bufferLength * 5 / 6, 10)
     const scaledWidth = ((canvas.width - width) / width)
-    ctx.beginPath()
-    ctx.moveTo(0, canvas.height)
-    for(let i = 0; i < bufferLength; i++) {
-      const height = canvas.height * dataArray[i] / 256
-      ctx.lineTo((scaledWidth * i), canvas.height - 1 - height)
-    }
-    ctx.lineTo(canvas.width, canvas.height)
-    // ctx.strokeStyle = '#0fb'
+    //
     ctx.fillStyle = '#0fb'
-    // ctx.fillStyle = '#2d2d2d'
-    // ctx.lineWidth = 10
-    // ctx.stroke()
-    ctx.fill()
+    spectrum(canvas, ctx, scaledWidth, 0)
+    ctx.fillStyle = '#2d2d2d'
+    spectrum(canvas, ctx, scaledWidth, 20)
   }
 }
