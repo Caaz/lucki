467dfbffe8c940b89e6354fdd552dd0fe8f31819
Daniel Caaz <daniel@caaz.me>
2017-01-08 06:40:54 -0800

    Oscilliscope scoping oscils.

--- src/view/visualizer/oscilliscope.js
+++ src/view/visualizer/oscilliscope.js
@@ -2,67 +2,28 @@ let bufferLength
 let data
 module.exports = {
   init({analyser}) {
-    analyser.fftSize = 256
+    analyser.fftSize = 2048 * 4
     bufferLength = analyser.frequencyBinCount
     data = new Uint8Array(bufferLength)
   },
   draw(timestamp, {analyser, canvas, ctx}) {
-    analyser.getByteFrequencyData(data)
-    ctx.lineWidth = 1
-    // const sliceWidth = canvas.width / (bufferLength - 1)
-    ctx.fillStyle = 'rgb(34, 34, 34)'
+    analyser.getByteTimeDomainData(data)
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.lineWidth = 2
+    ctx.strokeStyle = '#0fb'
     ctx.beginPath()
-    const sliceWidth = canvas.width / bufferLength
+
+    const sliceWidth = Number(canvas.width) * 1.0 / bufferLength
     let x = 0
-    let avg = 0
 
     for(let i = 0; i < bufferLength; i++) {
-      let v = data[i] / 128
-      if (data[i] > avg) avg = data[i]
-      v *= canvas.height / 2
-
-      if(i === 0) ctx.moveTo(x, v)
-      else ctx.lineTo(x, v)
+      const v = data[i] / 128.0
+      const y = v * canvas.height / 2
+      if(i === 0) ctx.moveTo(x, y)
+      else ctx.lineTo(x, y)
       x += sliceWidth
     }
-    console.log(avg)
     ctx.lineTo(canvas.width, canvas.height / 2)
     ctx.stroke()
   }
 }
-
-//   oscilliscope() {
-//     analyser.fftSize = 2048 * 4
-//     const bufferLength = analyser.frequencyBinCount
-//     const data = new Uint8Array(bufferLength)
-//     analyser.getByteTimeDomainData(data)
-// const dim = Math.min(canvas.width, canvas.height)
-// ctx.lineWidth = 1 / 600 * dim
-// const sliceWidth = dim / (bufferLength - 1)
-//
-// ctx.fillStyle = 'rgb(34, 34, 34)'
-//     ctx.fillRect(0, 0, canvas.width, canvas.height)
-//
-// ctx.beginPath()
-// // ctx.moveTo(0,-canvas.height/2);
-// ctx.save()
-// ctx.translate((canvas.width - dim) / 2, -canvas.height / 2)
-//
-// // ctx.fillStyle = 'rgb(45, 45, 45)'
-// // ctx.arc(dim / 2, canvas.height, dim / 2, 0, Math.PI * 2)
-// // ctx.fill()
-// // ctx.closePath()
-//
-// // ctx.beginPath()
-// for (let i = 0; i < bufferLength; i++) ctx.lineTo(sliceWidth * i, data[i] / 128 * canvas.height)
-// ctx.restore()
-// // ctx.translate(canvas.width / 2, canvas.height / 2)
-// // ctx.rotate(1 * Number(Math.PI) / 180)
-// // ctx.translate(-canvas.width / 2, -canvas.height / 2)
-// ctx.strokeStyle = 'rgb(0, 255, 187)'
-//
-// ctx.stroke()
-//     nextFrame()
-//   }
-//   // //
