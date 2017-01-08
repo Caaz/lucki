328a82285bb2cdcdc22024973e73c9e80db7faf3
Daniel Caaz <daniel@caaz.me>
2017-01-08 05:33:41 -0800

    Mirrored. Extra fancy.

--- src/view/visualizer/caaz.js
+++ src/view/visualizer/caaz.js
@@ -1,13 +1,22 @@
 let bufferLength
 let data
-function spectrum(canvas, ctx, width, offset) {
+function spectrum(canvas, ctx, width, scale) {
   ctx.beginPath()
-  ctx.moveTo(0, canvas.height + offset)
+  ctx.moveTo(0, canvas.height / 2)
   for(let i = 0; i < bufferLength; i++) {
-    const height = canvas.height * data[i] / 256
-    ctx.lineTo((width * i), canvas.height - 1 - height + offset)
+    const height = canvas.height / 2 * data[i] / 256
+    ctx.lineTo((width * i), canvas.height / 2 - 1 - height * scale)
   }
-  ctx.lineTo(canvas.width, canvas.height + offset)
+  ctx.lineTo(canvas.width, canvas.height / 2)
+  ctx.fill()
+  // if I were really fancy I'd continue it backwards...
+  ctx.beginPath()
+  ctx.moveTo(0, canvas.height / 2)
+  for(let i = 0; i < bufferLength; i++) {
+    const height = -(canvas.height / 2 * data[i] / 256)
+    ctx.lineTo((width * i), canvas.height / 2 - 1 - height * scale)
+  }
+  ctx.lineTo(canvas.width, canvas.height / 2)
   ctx.fill()
 }
 module.exports = {
@@ -19,16 +28,19 @@ module.exports = {
   draw(timestamp, {analyser, canvas, ctx}) {
     analyser.getByteFrequencyData(data)
     // draw previous frame
-    ctx.drawImage(canvas, 0, -1, canvas.width, canvas.height)
+    ctx.drawImage(canvas, 0, -10, canvas.width, canvas.height + 20)
     // darken it
-    ctx.fillStyle = 'rgba(0,0,0,0.05)'
+    ctx.globalCompositeOperation = 'multiply'
+    ctx.fillStyle = '#EEE'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     const width = parseInt(bufferLength * 5 / 6, 10)
     const scaledWidth = ((canvas.width - width) / width)
     //
+    ctx.globalCompositeOperation = 'source-over'
     ctx.fillStyle = '#0fb'
-    spectrum(canvas, ctx, scaledWidth, 0)
-    ctx.fillStyle = '#2d2d2d'
-    spectrum(canvas, ctx, scaledWidth, canvas.height / 10)
+    spectrum(canvas, ctx, scaledWidth, 1)
+    // ctx.fillStyle = '#2d2d2d'
+    ctx.fillStyle = '#000'
+    spectrum(canvas, ctx, scaledWidth, 0.6)
   }
 }
