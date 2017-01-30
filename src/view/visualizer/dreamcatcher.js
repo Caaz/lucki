39d485514412bd0d3788a557ffa8b1cc615573c5
Michael Bartlett <bartlettmic@gmail.com>
2017-01-30 00:47:48 -0700

    more fanciness, will make into dream catcher later!

--- src/view/visualizer/dreamcatcher.js
+++ src/view/visualizer/dreamcatcher.js
@@ -5,27 +5,30 @@ let cv
 let cap
 module.exports = {
   init({analyser, canvas}) {
-    analyser.fftSize = 512
+    analyser.fftSize = 2048
     bufferLength = analyser.frequencyBinCount
     data = new Uint8Array(bufferLength)
     cv = canvas
     ctx = cv.getContext('2d')
-    cap = data.length* 5 / 6
+    cap = data.length * 5 / 6
   },
   draw(timestamp, {analyser}) {
     analyser.getByteFrequencyData(data)
     // ctx.drawImage(cv, -10, 0, cv.width + 20, cv.height)
-    ctx.fillStyle = '#EEE'
+    ctx.fillStyle = '#888'
     ctx.fillRect(0, 0, cv.width, cv.height)
     ctx.save()
     ctx.translate(cv.width / 2, cv.height / 2)
-    ctx.fillStyle = '#777'
+    ctx.rotate(Math.PI)
     for (let i = 0; i < cap; i++) {
       ctx.rotate(Math.PI * 2 / cap)
       const theta = (i / cap) * Math.PI * 2
       const minrad = ellipse(theta, cv.width / 2, cv.height / 2)
       const maxrad = ellipse(theta, cv.width, cv.height) - minrad
       ctx.beginPath()
+      const color = (i / cap) * 360 - timestamp/20
+      ctx.fillStyle = 'hsl(' + color + ', 100%,50%)'
+      ctx.fillStyle = ''
       const radius = minrad + (data[i] / 256) * maxrad
       ctx.arc(0, radius, 2, 0, 2 * Math.PI)
       ctx.fill()
